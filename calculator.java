11fb749eff23357ac093c53bba031b0840f454cd
BHARATHI RAGAVENDRA <bharathiragavendra412@gmail.com>
2021-10-05 13:48:30 +0530

    first commit

---
+++ calculator.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class calculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\tCalculator");
+        System.out.println("Enter operator for operation i.e +,-,*,/.");
+        String str = input.next();
+        if(str.equals("+")||str.equals("-")||str.equals("*")||str.equals("/")){
+        System.out.print("Enter operands : ");
+           int a = input.nextInt();
+           int b = input.nextInt();
+           System.out.print("Result : ");
+           switch(str){
+                case "+" : 
+                          System.out.print(a+b);
+                          break;
+                case "-" : 
+                          System.out.print(a-b);
+                          break;
+                case "*" : 
+                          System.out.print(a*b);
+                          break;
+                case "/" : 
+                          System.out.print(a/b);
+                          break;              
+           }
+        }
+        else System.out.println("Invalid Operator");
+        input.close();
+    }
+}
+
